5e33152e67498f0266e7d4872bc99ef1491f5ce5
jhone <947023001@qq.com>
2019-11-04 16:35:31 +0800

    Initial commit

---
+++ app/src/main/java/com/darren/architect_day02/SectionAspect.java
@@ -0,0 +1,103 @@
+package com.darren.architect_day02;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+
+/**
+ * Created by hcDarren on 2017/8/27.
+ * 处理切点
+ */
+@Aspect
+public class SectionAspect {
+
+    /**
+     * 找到处理的切点
+     * * *(..)  可以处理所有的方法
+     */
+    @Pointcut("execution(@com.darren.architect_day02.CheckNet * *(..))")
+    public void checkNetBehavior() {
+
+    }
+
+    /**
+     * 处理切面
+     */
+    @Around("checkNetBehavior()")
+    public Object checkNet(ProceedingJoinPoint joinPoint) throws Throwable {
+        Log.e("TAG", "checkNet");
+        // 做埋点  日志上传  权限检测（我写的，RxPermission , easyPermission） 网络检测
+        // 网络检测
+        // 1.获取 CheckNet 注解  NDK  图片压缩  C++ 调用Java 方法
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        CheckNet checkNet = signature.getMethod().getAnnotation(CheckNet.class);
+        if (checkNet != null) {
+            // 2.判断有没有网络  怎么样获取 context?
+            Object object = joinPoint.getThis();// View Activity Fragment ； getThis() 当前切点方法所在的类
+            Context context = getContext(object);
+            if (context != null) {
+                if (!isNetworkAvailable(context)) {
+                    // 3.没有网络不要往下执行
+                    Toast.makeText(context,"请检查您的网络",Toast.LENGTH_LONG).show();
+                    return null;
+                }
+            }
+        }
+        return joinPoint.proceed();
+    }
+
+    /**
+     * 通过对象获取上下文
+     *
+     * @param object
+     * @return
+     */
+    private Context getContext(Object object) {
+        if (object instanceof Activity) {
+            return (Activity) object;
+        } else if (object instanceof Fragment) {
+            Fragment fragment = (Fragment) object;
+            return fragment.getActivity();
+        } else if (object instanceof View) {
+            View view = (View) object;
+            return view.getContext();
+        }
+        return null;
+    }
+
+    /**
+     * 检查当前网络是否可用
+     *
+     * @return
+     */
+    private static boolean isNetworkAvailable(Context context) {
+        // 获取手机所有连接管理对象（包括对wi-fi,net等连接的管理）
+        ConnectivityManager connectivityManager = (ConnectivityManager)
+                context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivityManager != null) {
+            // 获取NetworkInfo对象
+            NetworkInfo[] networkInfo = connectivityManager.getAllNetworkInfo();
+
+            if (networkInfo != null && networkInfo.length > 0) {
+                for (int i = 0; i < networkInfo.length; i++) {
+                    // 判断当前网络状态是否为连接状态
+                    if (networkInfo[i].getState() == NetworkInfo.State.CONNECTED) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
